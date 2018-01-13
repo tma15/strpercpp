f51612107879a7c3ebfa2a9c0a49a5519eaca7b0
tma15 <takuyamakino15@gmail.com>
2018-01-13 17:22:45 +0900

    Add namespace

--- src/node.cpp
+++ src/node.cpp
@@ -2,24 +2,12 @@
 
 #include "node.hpp"
 
+namespace strpercpp {
 Node::Node() {
-    this->Y = 0;
-//    this->bnext = nullptr;
-//    this->bnext = NULL;
-//    this->enext = NULL;
-    this->label = "";
-
-//    this->fids = std::unordered_set<int>();
-
-//    this->prev = nullptr;
-//    this->prev = NULL;
-//    this->next = NULL;
-
-    this->score = 0.;
-    this->path_score = 0.;
+  this->Y = 0;
+  this->label = "";
+  this->score = 0.;
+  this->path_score = 0.;
 };
 
-//void Node::add_bnext(Node& node) {
-//    this->bnext = &node;
-//};
-
+} // namespace strpercpp
