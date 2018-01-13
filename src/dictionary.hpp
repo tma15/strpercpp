adc8044df96598c0005e8f96033862b705439e4c
tma15 <takuyamakino15@gmail.com>
2018-01-13 15:35:49 +0900

    Add doc

--- src/dictionary.hpp
+++ src/dictionary.hpp
@@ -1,3 +1,6 @@
+/// \file dictionary.hpp
+/// \author Takuya Makino
+/// \date 2018/01/13
 #ifndef STRPERCPP_DICTIONARY_H
 #define STRPERCPP_DICTIONARY_H
 #include <string>
@@ -7,21 +10,57 @@
 namespace strpercpp {
     
 
+
+/*! @class Dictionary
+*   @brief Dictionary holds features for converting them into indice.
+*/
 class Dictionary {
-    private:
-        std::vector<std::string> itos;
-        std::unordered_map<std::string, int> stoi;
-    public:
-        Dictionary();
-        bool has(const std::string& key);
-        void add(const std::string& key);
-        std::string gets(const int id) const;
-        int geti(const std::string& key) const;
-        size_t size() const;
-        void save(const char* filename);
-        void save(FILE* fp);
-        void load(const char* filename);
-        void load(FILE* fp);
+  public:
+    Dictionary();
+
+    /*! checks whether key exists
+     *  \param[in] key
+     *  \param[out] has whether key exists or not
+     */
+    bool has(const std::string& key);
+
+    /*! adds key to Dictionary
+     *  \param[in] key
+     */
+    void add(const std::string& key);
+
+    /*! getsstring corresponding to id
+     *  \param[in] id the index of a feature
+     *  \param[out] key a feature
+     */
+    std::string gets(const int id) const;
+
+    /*! gets index corresponding to key
+     *  \param[in] key
+     *  \param[out] id
+     */
+    int geti(const std::string& key) const;
+
+    /*! gets the size of Dictionary
+     *  \param[out] size
+     */
+    size_t size() const;
+
+    /*! saves Dictionary
+     *  \param[in] filename
+     */
+    void save(const char* filename);
+    void save(FILE* fp);
+
+    /*! loads Dictionary
+     *  \param[in] filename
+     */
+    void load(const char* filename);
+    void load(FILE* fp);
+
+  private:
+    std::vector<std::string> itos;
+    std::unordered_map<std::string, int> stoi;
 };
 
 } // namespace strpercpp
--- src/node.hpp
+++ src/node.hpp
@@ -1,3 +1,6 @@
+/// \file node.hpp
+/// \author Takuya Makino
+/// \date 2018/01/13
 #ifndef STRPERCPP_NODE_H
 #define STRPERCPP_NODE_H
 
@@ -8,16 +11,30 @@
 
 namespace strpercpp {
   
+/*! @class Node
+ *  @brief Node represents a node in lattice.
+ */
 class Node {
   public:
+    /*! feature indice on the node */
     std::vector<int> feature_ids;
+
+    /*! label index */
     int Y;
+
+    /*! label */
     std::string label;
 
+    /*! the pointer to previous node */
     std::shared_ptr<Node> prev;
+
+    /*! the pointer to next node */
     std::shared_ptr<Node> bnext;
 
+    /*! the score of the node */
     float score;
+
+    /*! the cumulative score of the node from the start of lattice */
     float path_score;
 
     Node();
