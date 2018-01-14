54e2a580cf5c292f79542abe0fdd41314c0fbcb5
tma15 <takuyamakino15@gmail.com>
2018-01-14 14:07:52 +0900

    Change indent

--- src/viterbi.hpp
+++ src/viterbi.hpp
@@ -10,11 +10,13 @@ namespace strpercpp {
 
 void viterbi(std::vector< std::shared_ptr<Node> >& nodes);
 
-std::vector< std::shared_ptr<Node> > backtrack(std::vector< std::shared_ptr< Node > >& nodes);
+std::vector< std::shared_ptr<Node> >
+backtrack(std::vector< std::shared_ptr< Node > >& nodes);
 
-std::vector< std::shared_ptr< Node > > true_path(
-        std::vector< std::shared_ptr< Node > >& nodes,
-        const std::vector<int>& true_label_ids);
+std::vector< std::shared_ptr< Node > >
+true_path(std::vector< std::shared_ptr< Node > >& nodes,
+    const std::vector<int>& true_label_ids);
+
 
 } // namespace strpercpp
 
