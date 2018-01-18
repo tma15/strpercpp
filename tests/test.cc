f0c3f2aa3fb3a66ee985780f8a331e19b5ef3419
tma15 <takuyamakino15@gmail.com>
2018-01-18 21:05:28 +0900

    Add lattice test

--- tests/test.cc
+++ tests/test.cc
@@ -3,14 +3,45 @@
 #include "strpercpp/feature_template.hpp"
 #include "strpercpp/node.hpp"
 #include "strpercpp/structured_perceptron.hpp"
+#include "strpercpp/viterbi.hpp"
 
 using namespace strpercpp;
+
+void test_lattice() {
+  Node node;
+
+  int len_seq = 3;
+  std::vector<node_ptr> nodes(len_seq);
+
+  int num_label = 4;
+  for (int i=0; i < len_seq; ++i) {
+
+    node_ptr node(new Node());
+    node.get()->score = i;
+    nodes[i] = node;
+
+    for (int j=1; j < num_label; ++j) {
+      node_ptr node(new Node());
+      node.get()->score = j;
+
+      node_ptr next_node;
+      for (node_ptr n = nodes[i]; n != NULL; n = n.get()->bnext) {
+        next_node = n;
+      }
+      next_node->bnext = node;
+    }
+  }
+
+  viterbi(nodes);
+
+}
 
 int main(int argc, char const* argv[]) {
   Dictionary dict;
   FeatureTemplate tmpl;
-  Node node;
   StructuredPerceptron model;
+
+  test_lattice();
   
   return 0;
 }
