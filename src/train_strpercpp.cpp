329be7e7dedd1357fce6f1d2c59670760ca363b7
tma15 <takuyamakino15@gmail.com>
2018-01-24 22:49:06 +0900

    Replace getopt with ArgParser

--- src/train_strpercpp.cpp
+++ src/train_strpercpp.cpp
@@ -4,6 +4,7 @@
 #include "iostream"
 #include "string"
 
+#include "argparse.h"
 #include "corpus.hpp"
 #include "dictionary.hpp"
 #include "feature_template.hpp"
@@ -13,35 +14,38 @@ using namespace strpercpp;
 
 
 void usage() {
-  std::cout << "train_strpercpp -e <epoch> <train_data> <template_file> <model_file>";
+  std::cout << "train_strpercpp -e <epoch> -u <update_rule> <train_data> ";
+  std::cout << "<template_file> <model_file>";
   std::cout << std::endl;
 
-  std::cout << "  -e\tnumber of epoch";
+  std::cout << "  -e\tnumber of epoch" << std::endl;
+  std::cout << "  -u\tupdate rule" << std::endl;
+  std::cout << "\t0: full update" << std::endl;
+  std::cout << "\t1: early update" << std::endl;
   std::cout << std::endl;
 }
 
-int main(int argc, char* argv[]) {
-  int result;
-  int epoch = 3;
+int main(int argc, char const* argv[]) {
+  argparse::ArgParser parser;
+  parser.add_argument("-e", "3");
+  parser.add_argument("--update", "0");
+  parser.add_argument("train_file");
+  parser.add_argument("template_file");
+  parser.add_argument("model_file");
+  parser.parse_args(argc, argv);
 
-  while ((result=getopt(argc, argv, "he:")) != -1) {
-    switch (result) {
-      case 'h':
-        usage();
-        exit(0);
-      case 'e':
-        epoch = atoi(optarg);
-        break;
-    }
-  }
+  int epoch = parser.get<int>("e");
+  int update_rule = parser.get<int>("update");
+  std::string train_file = parser.get<std::string>("train_file");
+  std::string template_file = parser.get<std::string>("template_file");
+  std::string model_file = parser.get<std::string>("model_file");
 
-  int i = optind;
-  std::string train_file = std::string(argv[i]);
-  i += 1;
-  char* template_file = argv[i];
-  i += 1;
-  char* model_file = argv[i];
-
+//  printf("epoch:%d\n", epoch);
+//  printf("update_rule:%d\n", update_rule);
+//  printf("train_file:%s\n", train_file.c_str());
+//  printf("template_file:%s\n", template_file.c_str());
+//  printf("model_file:%s\n", model_file.c_str());
+//  exit(1);
 
   Dictionary feature_dic;
   Dictionary label_dic;
@@ -51,42 +55,42 @@ int main(int argc, char* argv[]) {
   std::vector< std::vector< node_ptr > > nodes_list;
   std::vector< std::vector< node_ptr > > true_path_list;
 
-  std::vector<FeatureTemplate> tmpl = read_template_file(template_file);
+  std::vector<FeatureTemplate> tmpl = read_template_file(template_file.c_str());
   Corpus corpus;
   bool train = true;
-  corpus.read(train_file,
-          &feature_dic, &label_dic,
-          &sequences, &labels);
+  corpus.read(train_file, &feature_dic, &label_dic, &sequences, &labels);
 
-  corpus.build_lattices(
-          &feature_dic, label_dic,
-          sequences, labels,
-          tmpl,
-          &nodes_list,
-          &true_path_list,
-          train);
-//  exit(0);
+  corpus.build_lattices(&feature_dic, label_dic,
+      sequences, labels, tmpl,
+      &nodes_list, &true_path_list, train);
 
   StructuredPerceptron perc(feature_dic, label_dic);
   perc.set_template(tmpl);
 
   std::cout << "#samples: " << sequences.size() << std::endl;
   std::cout << "#labels: " << label_dic.size() << std::endl;
-  std::cout << "#featres: " << feature_dic.size() << std::endl;
-//  exit(0);
+  std::cout << "#features: " << feature_dic.size() << std::endl;
+
   for (int e=0; e < epoch; ++e) {
     std::cout << "epoch:" << e+1 << "/" << epoch << std::endl;
     for (int i=0; i < nodes_list.size(); ++i) {
       std::vector< node_ptr > nodes = nodes_list[i];
       std::vector< node_ptr > true_path_ = true_path_list[i];
-      perc.fit(nodes, true_path_);
-//      perc.early_update(nodes, true_path_);
+
+      switch (update_rule) {
+        case 0:
+         perc.fit(nodes, true_path_);
+         break;
+        case 1:
+         perc.early_update(nodes, true_path_);
+         break;
+      }
     }
   }
 
   std::cout << "finish training" << std::endl;
 
-  perc.save(model_file);
+  perc.save(model_file.c_str());
 
 //    StructuredPerceptron perc2;
 //    perc2.load(model_file);
